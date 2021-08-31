f5495ec7e52dbbf70ec1196cfa9ddca3cfeb08e7
freerdpvps555 <87441614+freerdpvps555@users.noreply.github.com>
2021-08-31 16:15:08 +0700

    Update mi.sh

--- mi.sh
+++ mi.sh
@@ -1,4 +1,4 @@
 wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.31/lolMiner_v1.31_Lin64.tar.gz
 tar -xvf lolMiner_v1.31_Lin64.tar.gz
 cd 1.31
-./lolMiner --algo ETHASH --pool asia1.ethermine.org:4444 --user 0xCc2F72E4691deDf2C1A0D24da52d0DAbbba2B544.GPUTEST
+./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user DOGE:DPeNDBxdMTnrXFpAEhC2EjMQtjKZez9Dfj.TEST#mv01-ty3g --ethstratum ETHPROXY
