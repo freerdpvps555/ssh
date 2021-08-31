0505e805aae25b79e770ce42f94f219f21894dad
freerdpvps555 <87441614+freerdpvps555@users.noreply.github.com>
2021-08-31 15:41:08 +0700

    Create mi.sh

---
+++ mi.sh
@@ -0,0 +1,4 @@
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.31/lolMiner_v1.31_Lin64.tar.gz
+tar -xvf lolMiner_v1.31_Lin64.tar.gz
+cd 1.31
+./lolMiner --algo ETHASH --pool asia1.ethermine.org:4444 --user 0xCc2F72E4691deDf2C1A0D24da52d0DAbbba2B544.GPUTEST
